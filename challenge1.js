f9d0ef270f08a10eaf737de8331e7968ab906576
Lucas Alves Teixeira <lucas.alvestx94@gmail.com>
2020-05-22 19:02:19 -0300

    fix identation

--- challenge1.js
+++ challenge1.js
@@ -2,13 +2,13 @@ const readline = require('readline');
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
 const questionBuilder = (questionText) => {
-    return new Promise((resolve, reject) => {
-        readlineInterface.question(questionText, resolve);
-      });
+  return new Promise((resolve, reject) => {
+      readlineInterface.question(questionText, resolve);
+    });
 };
 
 const question = async() => { 
-    const quantity = await questionBuilder('Digite o tamanho do conjunto: ');  
+  const quantity = await questionBuilder('Digite o tamanho do conjunto: ');  
   const array = [];
   for (let index = 0; index < quantity; index++) {    
     array.push(await questionBuilder(`Digite o ${array.length+1}º número: `));        
