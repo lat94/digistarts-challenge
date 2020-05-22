5e24e37acef734422a9a841126a71b70ac12d68c
Lucas Alves Teixeira <lucas.alvestx94@gmail.com>
2020-05-22 19:10:12 -0300

    change default identation to two spaces

--- challenge1.js
+++ challenge1.js
@@ -3,15 +3,15 @@ const readlineInterface = readline.createInterface(process.stdin, process.stdout
 
 const questionBuilder = (questionText) => {
   return new Promise((resolve, reject) => {
-      readlineInterface.question(questionText, resolve);
-    });
+    readlineInterface.question(questionText, resolve);
+  });
 };
 
-const question = async() => { 
-  const quantity = await questionBuilder('Digite o tamanho do conjunto: ');  
+const question = async () => {
+  const quantity = await questionBuilder('Digite o tamanho do conjunto: ');
   const array = [];
-  for (let index = 0; index < quantity; index++) {    
-    array.push(await questionBuilder(`Digite o ${array.length+1}º número: `));        
+  for (let index = 0; index < quantity; index++) {
+    array.push(await questionBuilder(`Digite o ${array.length + 1}º número: `));
   }
   const group = Array.from(new Set(array.sort()));
   console.log(`O seu conjunto de elementos único é: K[${group}]`);
